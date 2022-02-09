2ff02ed2d397d7cc9e44e0bc853379fc1a71fc56
Junaed <junaed1995@gmail.com>
2022-02-09 21:20:49 +0600

    Initial commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/bird.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.lightBlueAccent,
+              child: Bird(),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
